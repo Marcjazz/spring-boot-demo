518a97323c95fff94e665da1d5a74506e51d844e
Marco Kuidja <kuidjamarco@gmail.com>
2024-04-13 11:09:35 +0100

    expose person CRUD APIs

--- demo/src/main/java/com/example/demo/api/PersonController.java
+++ demo/src/main/java/com/example/demo/api/PersonController.java
@@ -2,11 +2,13 @@ package com.example.demo.api;
 
 import com.example.demo.model.Person;
 import com.example.demo.service.PersonService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("/api/v1/person")
 @RestController
@@ -19,7 +21,27 @@ public class PersonController {
     }
 
     @PostMapping
-    public void addPerson(@RequestBody Person person) {
-        this.personService.addPerson(person);
+    public int addPerson(@Valid @NonNull @RequestBody Person person) {
+        return this.personService.addPerson(person);
+    }
+
+    @GetMapping
+    public List<Person> getPeople() {
+        return this.personService.getPeople();
+    }
+
+    @GetMapping(path = "{id}")
+    public Person getPersonById(@PathVariable("id") UUID id) {
+        return this.personService.getPersonById(id).orElse(null);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public int deletePersonById(@PathVariable("id") UUID id) {
+        return this.personService.deletePersonById(id);
+    }
+
+    @PutMapping(path = "{id}")
+    public int updatePersonById(@PathVariable("id") UUID id, @Valid @NonNull @RequestBody Person person) {
+        return this.personService.updatePersonById(id, person);
     }
 }
--- demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
+++ demo/src/main/java/com/example/demo/dao/FakePersonDataAccessService.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Person;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.List;
 
@@ -16,4 +17,37 @@ public class FakePersonDataAccessService implements PersonDao {
         DB.add(new Person(id, person.getName()));
         return 1;
     }
+
+    @Override
+    public List<Person> getPeople() {
+        return DB;
+    }
+
+    @Override
+    public Optional<Person> getPersonById(UUID id) {
+        return DB.stream().filter(person -> person.getId().equals(id)).findFirst();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        Optional<Person> optionalPerson = getPersonById(id);
+        if(optionalPerson.isEmpty()) {
+            return 0;
+        }
+        DB.remove(optionalPerson.get());
+        return 1;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person payload) {
+        return getPersonById(id).map(person -> {
+            int indexOfPersonToUpdate =  DB.indexOf(person);
+            if(indexOfPersonToUpdate >= 0) {
+                DB.set(indexOfPersonToUpdate, new Person(id, payload.getName()));
+                return 1;
+            }
+            return  0;
+        }).orElse(0);
+    }
+
 }
