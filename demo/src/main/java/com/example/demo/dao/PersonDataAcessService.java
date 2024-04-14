9ea1aa155f5c630837ae053702b0cfcfaced870e
Marco Kuidja <kuidjamarco@gmail.com>
2024-04-14 16:16:19 +0100

    added person migration file and implemented fetch APIs endpoints

--- demo/src/main/java/com/example/demo/dao/PersonDataAcessService.java
+++ demo/src/main/java/com/example/demo/dao/PersonDataAcessService.java
@@ -1,6 +1,9 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -8,7 +11,13 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository("postgres")
-public class PersonDataAcessService implements  PersonDao {
+public class PersonDataAcessService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    PersonDataAcessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,12 +25,24 @@ public class PersonDataAcessService implements  PersonDao {
 
     @Override
     public List<Person> getPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
     public Optional<Person> getPersonById(UUID id) {
-        return Optional.empty();
+        final String sql = "SELECT id, name FROM person WHERE id = ?";
+
+        Person person = jdbcTemplate.queryForObject(sql, (resultSet, i) -> {
+            UUID personId = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(personId, name);
+        }, id);
+        return Optional.ofNullable(person);
     }
 
     @Override
